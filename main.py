66d717cddb458536156308cd1e476f353d71cc80
Savitskiy Andrey <stud.ansav27@yandex.ru>
2021-12-15 20:43:46 +0300

    Add venv sql libs

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import mysql.connector
+from mysql.connector import connect, Error
+
+
+try:
+    with connect(
+        host="localhost",
+        user='root',
+        password='D2p96oMJ8Yrc1HDhSZDe',
+        database="polyclinic",
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
